1b5978525ed031b45e9249bde67f15404aec5e90
Sky Transport <skytransport@Skys-MacBook-Pro.local>
2018-12-13 11:10:07 +0100

    checked the whole page

--- javascript/redmenu.js
+++ javascript/redmenu.js
@@ -1,11 +1,17 @@
-let burger = document.querySelector(".burger");
-let red = document.querySelector(".red");
+let menu = document.querySelector(".menu");
+let nav = document.querySelector("nav");
 let photo = document.querySelector("#photo");
 let titles = document.querySelector(".redlist");
 
-burger.addEventListener("click", popup);
+menu.addEventListener("click", popup);
+
+
 function popup(){
-    red.classList.toggle("open");
+    console.log(this)
+    nav.classList.toggle("open");
     //input.classList.toggle("active");
     //bt2.classList.toggle("active");
 }
+
+
+
